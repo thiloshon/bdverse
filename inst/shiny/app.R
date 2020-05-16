13536e8c8cd657b5278087dc87099ff1c57968c1
Thiloshon Nagarajah <thiloshon@gmail.com>
2020-05-16 21:16:56 +0530

    upstream sync

--- inst/shiny/app.R
+++ inst/shiny/app.R
@@ -1,7 +1,7 @@
 library(shiny)
 library(rstudioapi)
 
-# UI
+
 ui <- fluidPage(fluidRow(
     tags$head(
         tags$meta(name="viewport", content="width=device-width, initial-scale=1.0"),
@@ -62,13 +62,12 @@ ui <- fluidPage(fluidRow(
                 class = "decr",
                 "Visualize and preview different aspects of biodiversity data."
             ),
-            actionButton("action", label = "Under Development"),
+            actionButton("action", label = "Coming Soon"),
             img(src = '001.png', align = "center")
         )
     )
 ))
 
-# Server
 server <- function(input, output) {
     observeEvent(input$bddwc, {
         path_app <- system.file("scripts", 'bddwc.R', package = "bdverse")
